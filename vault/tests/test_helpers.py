0d2510c051430c5bad9bf6219ed8e894dc7c31e3
Tim Loyer <tloyer@apps.directemployers.org>
2021-03-17 16:05:47 -0400

    add tests for json parsing

--- vault/tests/test_helpers.py
+++ vault/tests/test_helpers.py
@@ -1,10 +1,11 @@
 from base64 import b64encode
+from json import dumps
 from unittest import TestCase
 
-from vault import _is_base64
+from vault import _is_base64, _parse_json_secrets
 
 
-class SecretParsingTest(TestCase):
+class HelperFunctionsTest(TestCase):
     def test_is_base64(self):
         test_string = "this will be base64 encoded"
         test_encoded = b64encode(bytes(test_string, "utf-8")).decode("utf-8")
@@ -13,3 +14,28 @@ class SecretParsingTest(TestCase):
     def test_is_not_base64(self):
         test_string = "this will be base64 encoded"
         self.assertFalse(_is_base64(test_string))
+
+    def test_json_parsing(self):
+        """
+        JSON objects may by simple key -> value objects, or adhere to some form of
+        Vault's own Secret struct format (which nests object content in `data` objects).
+        """
+        plain_secret = {"SECRET_KEY": "secret value"}
+        data_secret = {"data": plain_secret}
+        ddata_secret = {"data": data_secret}
+
+        # Not a thing, should yield a dict that looks like {data: {SECRET_KEY: ...}}.
+        dddata_secret = {"data": ddata_secret}
+
+        # Tests expected test cases where secrets are either top level or under up to
+        # two levels of `data` keys.
+        for secrets in [plain_secret, data_secret, ddata_secret]:
+            secrets = dumps(secrets)
+            parsed = _parse_json_secrets(secrets)
+            self.assertEqual(plain_secret, parsed)
+
+        # Tests the case where our expected secrets are under three levels of `data`
+        # keys, which would result in `data` being a top-level dictionary key.
+        secrets = dumps(dddata_secret)
+        parsed = _parse_json_secrets(secrets)
+        self.assertEqual(data_secret, parsed)
--- vault/tests/test_secret_handling.py
+++ vault/tests/test_secret_handling.py
@@ -4,6 +4,7 @@ from unittest.mock import patch
 
 from vault.tests.data import de_test_secrets
 from vault.tests.helpers import override_env
+from vault import _parse_json_secrets
 
 TEST_DATA_PATH = Path.cwd().joinpath("data")
 TEST_SECRET = "shhhhh!"
@@ -13,6 +14,11 @@ class SecretLoaderTest(TestCase):
     @override_env({"VAULTPY_ENABLE_VAULT": "false", "VAULTPY_ENABLE_DATADOG": "false"})
     @patch("vault.import_module")
     def test_de_secrets_module(self, mock_import):
+        """
+        Test that vaultpy can get secrets from de_secrets.py when
+        VAULTPY_ENABLE_VAULT is false. For testing purposes, de_secrets module is
+        mocked from tests/data/de_test_secrets.py for this test.
+        """
         from vault import secrets
 
         mock_import.return_value = de_test_secrets
@@ -26,6 +32,9 @@ class SecretLoaderTest(TestCase):
         }
     )
     def test_json_secrets(self):
+        """
+        Test injected secrets that are JSON formatted.
+        """
         from vault import secrets
 
         self.assertEqual(TEST_SECRET, secrets.SUPER_SECRET_KEY)
@@ -38,6 +47,9 @@ class SecretLoaderTest(TestCase):
         }
     )
     def test_json_base64_secrets(self):
+        """
+        Test injected secrets that are JSON formatted and Base 64 encoded.
+        """
         from vault import secrets
 
         self.assertEqual(TEST_SECRET, secrets.SUPER_SECRET_KEY)
