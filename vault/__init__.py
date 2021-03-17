a77d7db4fddffa7c5be8771a862e25f2f44181d1
Tim Loyer <tloyer@apps.directemployers.org>
2021-03-17 16:06:09 -0400

    add comments and refactor json parsing into separate function for easier testing

--- vault/__init__.py
+++ vault/__init__.py
@@ -2,18 +2,18 @@ import logging
 from base64 import b64decode, b64encode
 from importlib import import_module
 from json import loads
-from typing import Dict
+from typing import Callable, Dict
 
 from datadog import statsd
 
-from . import config
+from vault import config
 
 logger = logging.getLogger(__name__)
 
 
-def _is_base64(s):
+def _is_base64(s: str) -> bool:
     """
-    Checks whether a sting has been base 64 encoded.
+    Checks whether a string has been base 64 encoded.
     """
     try:
         return b64encode(b64decode(s)) == bytes(s, "utf-8")
@@ -41,8 +41,15 @@ def _load_vault_secrets() -> Dict:
     if _is_base64(contents):
         contents = b64decode(contents)
 
-    contents = loads(contents)
+    return _parse_json_secrets(contents)
 
+
+def _parse_json_secrets(json_contents: str) -> Dict:
+    """
+    Handles regular JSON strings and also JSON strings that adhere to Vault's secret
+    struct format by looking for `data` objects to access.
+    """
+    contents = loads(json_contents)
     try:
         # If the injected secret is formatted as a Vault Secret struct,
         # the actual secrets will be nested under two `data` keys.
@@ -59,6 +66,9 @@ def _get_secrets() -> Dict:
     Get secrets from de_secrets.py in local dev, or from Vault injected secrets file
     located at path in VAULT_SECRETS_PATH. Performs base 64 decode followed by JSON
     decode on file contents.
+
+    This is passed to VaultSecretsWrapper as a callable to delay loading of files
+    and modules until configuration has been set in code.
     """
     if not config.ENABLE_VAULT:
         # Use dev secrets when available.
@@ -72,9 +82,20 @@ class VaultSecretsWrapper:
     Provide access to secrets as attributes and send secret-usage analytics to Datadog.
     """
 
-    def __init__(self, secrets: Dict):
-        self._keys = secrets.keys()
+    def __init__(self, secrets_loader: Callable):
+        """
+        VaultSecretsWrapper takes a callable (secrets_loader) which will return a
+        dictionary of secrets when called. This is to prevent vaultpy from fully
+        initializing until the first secret it returned, which prevents code running
+        before the configuration has been set (i.e. in testing).
+        """
+        self._secrets_loader = secrets_loader
+        self._keys = None
 
+    def _load_secrets(self):
+        secrets = self._secrets_loader()
+
+        self._keys = secrets.keys()
         for key, value in secrets.items():
             # Set baseline usage of 0 for all secrets.
             self._record_usage(key)
@@ -101,7 +122,12 @@ class VaultSecretsWrapper:
         Override the default getattribute method so that we can track secret key
         usage with Datadog. Non-secret attributes are passed on to the default method.
         """
-        if key != "_keys" and key in self._keys:
+        ignored_keys = ["_keys", "_load_secrets", "_secrets_loader"]
+
+        if key not in ignored_keys and self._keys is None:
+            self._load_secrets()
+
+        if key not in ignored_keys and key in self._keys:
             try:
                 self._record_usage(key, 1)
                 return super().__getattribute__(key)
@@ -112,5 +138,5 @@ class VaultSecretsWrapper:
         return super().__getattribute__(key)
 
 
-secrets = VaultSecretsWrapper(_get_secrets())
+secrets = VaultSecretsWrapper(_get_secrets)
 __all__ = ("secrets",)
